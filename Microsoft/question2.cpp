16969eb7be04fb06ae524b425b24c068471f8d93
Tong Chen <tongxyh@163.com>
2017-07-20 10:20:10 +0800

    Question 2 can swap correct,but spend more steps

--- Microsoft/question2.cpp
+++ Microsoft/question2.cpp
@@ -12,54 +12,54 @@ int main(void) {
     string a,b;
     int end_flag = 0;
     while(n--) {
-        int tmp_i,tmp_j=0;
+        long int tmp_i,tmp_j=0;
         char tmp;
         long int count = 0;
         cin >> a >> b;
-        cout << b.size() << endl;
+        //cout << b.size() << endl;
         for(int i=0;i<b.size();i++){
-            cout << i << endl;
+            //cout << "String B: "<< i << endl;
             if(end_flag == 1)
                 break;
-            if(b[i] == a[i])
-                    continue;
-            for(int j=0;j<a.size();j++){
-                cout << j << endl;
+            if(b[i] == a[i]){
+            //    cout << "Same" << endl;
+                continue;
+            }
+            for(int j=i;j<a.size();j++){
+            //    cout << j << endl;
                 if (b[i] == a[j] && a[i] == b[j] && i != j){
-                    cout << "MARK" << endl;
+                    cout << "MARK1" << " ";
                     tmp = a[i];
                     a[i] = a[j];
                     a[j] = tmp;
-                    cout << i << j << a << b << endl;
+                    cout << i << " "<< j <<" "<< a << " " << b << endl;
                     count = count + 1;
                     if(strcmp(a.c_str(),b.c_str())==0){
-                        cout << count;
+                        cout << count << endl;
                         end_flag = 1;
                         break;
                     }
                     break;
                 }
                 if(b[i] == a[j] && i != j){
-                    cout << "MARK2" << endl;
+            //        cout << "MARK2" << " ";
                     tmp_i = i;
                     tmp_j = j;
-                    cout << i << j << endl;
+            //        cout << i << j << endl;
                 }
-                if(tmp_i >= 0 && tmp_j >= 0){
-                    cout << "MARK3" << endl;
-                    cout << i << j << a[tmp_i] << endl;
+            }
+                if(tmp_i >= 0 && tmp_j >= 0 && end_flag == 0){
                     tmp = a[tmp_i];
-                    cout << "MARK34" << endl;
+                    cout << "MARK3" << " ";
                     a[tmp_i] = a[tmp_j];
                     a[tmp_j] = tmp;
                     count = count + 1;
-                    cout << i << j << a << b << endl;
+                    cout << tmp_i << " "<< tmp_j <<" "<< a << " " << b << endl;
                     if(strcmp(a.c_str(),b.c_str())==0){
-                        cout << count;
+                        cout << count << endl;
                         end_flag = 1;
                     }
                 }
-            }
                 }
         }
     }
