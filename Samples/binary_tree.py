a1ae327b6ae89608b7315024c69200d672263e72
Tong Chen <tongxyh@163.com>
2017-08-01 10:03:13 +0800

    add pre,mid,after traverse

--- Samples/binary_tree.py
+++ Samples/binary_tree.py
@@ -5,3 +5,24 @@ class Node(object):
         self.elem = elem
         self.lchild = lchild
         self.rchild = rchild
+
+def preTraverse(root):  
+    if root==None:  
+        return  
+    print(root.value)  
+    preTraverse(root.left)  
+    preTraverse(root.right)  
+  
+def midTraverse(root):  
+    if root==None:  
+        return  
+    midTraverse(root.left)  
+    print(root.value)  
+    midTraverse(root.right)  
+  
+def afterTraverse(root):  
+    if root==None:  
+        return  
+    afterTraverse(root.left)  
+    afterTraverse(root.right)  
+    print(root.value) 
